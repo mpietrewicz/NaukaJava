90f200d7f27cb523b5d6b91cfafcfc9898410613
Marek <mpietrewicz@gmail.com>
2017-06-28 23:58:27 +0200

    Przygotowanie repozytorium

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package HelloWorld;
+
+/**
+ * Created by Marek on 2017-06-28.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
